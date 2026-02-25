8feb7c16093d0606ba093e8b06471119a67a83fe
Ehara <mnrehr0913@gmail.com>
2026-02-25 16:21:22 +0900

    index.php →news、menu部分php適用 news_detail.php , news.php → php適用 db_info.php , function.php 作成

--- inc/db_info.php
+++ inc/db_info.php
@@ -1,6 +1,6 @@
 <?php
 // DBの接続情報
 define('DB_HOST', 'localhost');
-define('DB_NAME', '');
-define('DB_USER', '');
-define('DB_PASS', '');
+define('DB_NAME', 'gyoza_giga');
+define('DB_USER', 'gyouza_giga');
+define('DB_PASS', 'gigagiga1234');
