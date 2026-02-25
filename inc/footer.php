5e4016b5fa4526783af61a631770a0f2d738d55d
fuku <fuku12maturika2000@gmail.com>
2026-02-25 14:37:28 +0900

    fotter.phpにfotter要素のhtmlをコピーしました。

---
+++ inc/footer.php
@@ -0,0 +1,31 @@
+<?php
+$url = 'file:///C:/xampp/htdocs/gyouza_fes__Team-Giga/index.html';
+$html = file_get_contents($url);
+?>
+
+<footer class="l-footer">
+    <div class="l-fotter__contact">
+        <a href="contact.html" class="c-btn--black"><img src="img/icon-mail.png" alt="">
+            <p class="">Contact</p>
+        </a>
+        <a href="tel:XXX-XXX-XXX" class="c-btn--black"><img src="img/icon-tel.png" alt="">
+            <p>XXX-XXX-XXX</p>
+        </a>
+    </div>
+    <ul class="l-footer__sns">
+        <li class="c-sns__link"><a href="#"><img src="img/sns-x.png" alt=""></a></li>
+        <li class="c-sns__link"><a href="#"><img src="img/sns-insta.png" alt=""></a></li>
+        <li class="c-sns__link"><a href="#"><img src="img/sns-facebook.png" alt=""></a></li>
+    </ul>
+    <div class="l-footer__text">
+        <div class="c-footer__company">
+            <p>主催：ふくおか餃子FES実行委員会</p>
+            <p>製作協力：創造社リカレントスクール福岡校</p>
+            <p>協賛：九州餃子部</p>
+        </div>
+        <a href="privacy.html">個人情報保護方針</a>
+        <small>&copy;2030 福岡餃子FES実行委員会</small>
+    </div>
+</footer>
+<a href="#top" class="c-btn__top"><img src="./img/back-top.png" alt="topへ戻る"></a>
+</body>
