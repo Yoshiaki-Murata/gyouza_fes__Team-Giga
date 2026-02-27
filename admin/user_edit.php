1b17e47129f64595f1051125708536e45b9a8b77
Yoshiaki Murata <mrtysak.0517@gmail.com>
2026-02-27 14:03:02 +0900

    git_edit完成しました

---
+++ admin/user_edit.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+require_once '../inc/function.php';
+
+try {
+    $db = db_connect();
+    $sql2 = "SELECT * FROM roles";
+    $stmt2 = $db->prepare($sql2);
+    $stmt2->execute();
+    $result = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    exit("エラー" . $e->getMessage());
+}
+
+// POST送信方受け取ったIDとおなじユーザー情報を取得
+if (!empty($_POST)) {
+    if (!empty($_POST["id"])) {
+        $userid = (int)$_POST["id"];
+        try {
+            $db = db_connect();
+            $sql = "SELECT * FROM users WHERE id = :userid";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":userid", $userid, PDO::PARAM_INT);
+            $stmt->execute();
+            $target="";
+            $target=$stmt->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            exit("エラー" . $e->getMessage());
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+    <title>ユーザー編集</title>
+</head>
+<body>
+     <main role="main" class="container" style="padding:60px 15px 0">
+        <h1 class="my-5 text-center">ユーザー編集</h1>
+        <form action="./user_edit_do.php" method="post">
+            <div class="row justify-content-center">
+                <div class="mb-3 col-6">
+                    <label for="username" class="form-label">ユーザー名（半角英数８文字以上）</label>
+                    <input type="text" name="username" id="username" class="form-control" value="<?php echo $target["username"]; ?>">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="mb-5 col-6">
+                    <label for="password" class="form-label">パスワード（半角英数８文字以上）</label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="mb-5 col-6">
+                    <label for="role_id" class="form-label">権限</label>
+                    <select name="role_id" id="role_id" class="form-select form-select-sm mb-5" aria-label="Small select example">
+                        <?php foreach ($result as  $row): ?>
+                            <option value="<?php echo $row["id"]; ?>" <?php echo $row["id"]===$target["role_id"]? "selected":"" ?>>
+                                <?php echo $row["role"]; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="mb-5 text-center">
+                <input type="hidden" name="id" value="<?php echo $target["id"]; ?>">
+                <input type="submit" value="登録" class="btn btn-primary">
+            </div>
+        </form>
+    </main>
+</body>
+</html>
---
+++ admin/user_edit_do.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+require_once '../inc/function.php';
+
+check_array($_POST);
+
+if (!empty($_POST)) {
+    if (!empty($_POST["username"]) && !empty($_POST["role_id"]) && !empty($_POST["id"])) {
+        $username = $_POST["username"];
+        $role_id = (int)$_POST["role_id"];
+        $id = (int)$_POST["id"];
+        $password = $_POST["password"];
+
+        // ユーザー名が半角英数８文字以上か確認
+        if (!preg_match("/^[a-zA-Z0-9_-]{8,}$/", $username)) {
+            header("location:user_add.php");
+            exit();
+        }
+        // パスワードの変更があるときは半角英数８文字以上か確認
+        if (!empty($password)) {
+            if (!preg_match("/^[a-zA-Z0-9_-]{8,}$/", $password)) {
+                header("location:user_add.php");
+                exit();
+            }
+        }
+
+        // ユーザー名が重複していないか確認
+        try {
+            // DB接続～同じ名前が無いか確認
+            $db = db_connect();
+            $sql = "SELECT COUNT(username) FROM users WHERE username = :username AND id != :id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":username", $username, PDO::PARAM_STR);
+            $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_NUM); 
+            // 同じ名前があったらuser_add.phpに返す。
+            if ($result[0] !== 0) {
+                header("location:user_add.php");
+                exit();
+            }
+
+            // 変更内容をDBに修正する
+            // パスワードの変更があるときはパスワードハッシュ化する
+            if (!empty($password)) {
+                $password_hash = password_hash($password, PASSWORD_DEFAULT);
+            }
+            // パスワードがあるときとない時のSQL文を分岐する
+            if (!empty($password)) {
+                $sql2 = "UPDATE users SET username=:username,password=:password,role_id=:role_id WHERE id=:id";
+            } else {
+                $sql2 = "UPDATE users SET username=:username,role_id=:role_id WHERE id=:id";
+            }
+
+            $stmt2 = $db->prepare("$sql2");
+            $stmt2->bindParam(":username", $username, PDO::PARAM_STR);
+            // パスワードの変更があるかチェック
+            if (!empty($password)) {
+                $stmt2->bindParam(":password", $password_hash, PDO::PARAM_STR);
+            }
+            $stmt2->bindParam(":role_id", $role_id, PDO::PARAM_INT);
+            $stmt2->bindParam(":id",$id,PDO::PARAM_INT);
+            $stmt2->execute();
+            header("location:user.php");
+            exit();
+        } catch (PDOException $e) {
+            exit("エラー" . $e->getMessage());
+        }
+    }
+}
