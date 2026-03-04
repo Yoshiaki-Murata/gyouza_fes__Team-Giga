414ba594f9ed7b3efeddeaf09f31f1bbe00ac87a
Yoshiaki Murata <mrtysak.0517@gmail.com>
2026-03-04 11:21:20 +0900

    ナブバーのリンク表示の不具合修正

--- admin/menu_edit.php
+++ admin/menu_edit.php
@@ -34,12 +34,15 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@4.0.1/destyle.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Noto+Sans+JP:wght@100..900&family=Zen+Maru+Gothic&display=swap"
         rel="stylesheet">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="../css/style.css">
     <title>商品編集・登録</title>
 </head>
