fd43508cb6d6b690f413b3abb7eaf6e2637c34ef
fuku <fuku12maturika2000@gmail.com>
2026-03-05 11:34:11 +0900

    session_start();を追加

--- admin/shop_edit.php
+++ admin/shop_edit.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once __DIR__ . '/../inc/function.php';
 
 // TODO: ID取得とバリデーション
--- admin/shop_edit_do.php
+++ admin/shop_edit_do.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../inc/function.php';
 session_start();
+require_once __DIR__ . '/../inc/function.php';
 
 $id = (int)($_POST['id'] ?? 0);
 
